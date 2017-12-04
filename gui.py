afff0a53f337ccf787cd4426fc0ea6860c9d90a7
Malek Mustapha-Abdullah <malek.mustaphaabdullah@uoit.net>
2017-12-04 13:27:12 -0500

    Update gui.py

--- gui.py
+++ gui.py
@@ -16,16 +16,32 @@ def select_all(conn, table):
 	cur.execute("SELECT * FROM " + table + ";")
 	rows = cur.fetchall()
 
-	return rows(0)
+	return rows[len(rows)]
 
 def main():
 	database = "IoT.db"
+	window = Tk()
+	window.title("Sensorian Data")
 
 	conn = init_conn(database)
 
 	with conn:
+		to_display = select_all(conn, "Temperature")
+		Label(window, text = "Temperature: ", bg="none", fg="black", font="none 12 bold") .grid(row=1, column = 0, sticky=W)
+		Label(window, text = to_display, bg="none", fg="black", font="none 12") .grid(row=1, column = 1, sticky=W)
 		
-
+		to_display = select_all(conn, "Light")
+		Label(window, text = "Light: ", bg="none", fg="black", font="none 12 bold") .grid(row=2, column = 0, sticky=W)
+		Label(window, text = to_display, bg="none", fg="black", font="none 12") .grid(row=2, column = 1, sticky=W)
+		
+		to_display = select_all(conn, "Pressure")
+		Label(window, text = "Pressure: ", bg="none", fg="black", font="none 12 bold") .grid(row=3, column = 0, sticky=W)
+		Label(window, text = to_display, bg="none", fg="black", font="none 12") .grid(row=3, column = 1, sticky=W)
+		
+		to_display = select_all(conn, "Altitude")
+		Label(window, text = "Altitude: ", bg="none", fg="black", font="none 12 bold") .grid(row=4, column = 0, sticky=W)
+		Label(window, text = to_display, bg="none", fg="black", font="none 12") .grid(row=4, column = 1, sticky=W)
+		
 
 if __name__ == '__main__':
     main()
