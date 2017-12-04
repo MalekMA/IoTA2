1dbf10977ed7c37075c2ae70f526cdd6008f2fbf
Malek Mustapha-Abdullah <malek.mustaphaabdullah@uoit.net>
2017-12-04 15:02:19 -0500

    Update gui.py

--- gui.py
+++ gui.py
@@ -1,47 +1,53 @@
 import sqlite3
 from sqlite3 import Error
-from tkinter import *
+#from tkinter import *
 
-def init_conn(db)
-	try:
-		conn = sqlite3.connect(db)
-		return conn
-	except Error as e:
-		print(e)
+def init_conn(db):
+        try:
+                conn = sqlite3.connect(db)
+                return conn
+        except Error as e:
+                print(e)
 
-	return None
+        return None
 
 def select_all(conn, table):
-	cur = conn.cursor()
-	cur.execute("SELECT * FROM " + table + ";")
-	rows = cur.fetchall()
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM " + table + ";")
+        rows = cur.fetchall()
+        row = len(rows) - 1
+        return rows[row]
 
-	return rows[len(rows)]
+def select_table(conn, table):
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM " + table + ";")
+        rows = cur.fetchall()
+        return rows
 
 def main():
-	database = "IoT.db"
-	window = Tk()
-	window.title("Sensorian Data")
+        database = "IoT.db"
+        #window = Tk()
+        #window.title("Sensorian Data")
 
-	conn = init_conn(database)
+        conn = init_conn(database)
 
-	with conn:
-		to_display = select_all(conn, "Temperature")
-		Label(window, text = "Temperature: ", bg="none", fg="black", font="none 12 bold") .grid(row=1, column = 0, sticky=W)
-		Label(window, text = to_display, bg="none", fg="black", font="none 12") .grid(row=1, column = 1, sticky=W)
-		
-		to_display = select_all(conn, "Light")
-		Label(window, text = "Light: ", bg="none", fg="black", font="none 12 bold") .grid(row=2, column = 0, sticky=W)
-		Label(window, text = to_display, bg="none", fg="black", font="none 12") .grid(row=2, column = 1, sticky=W)
-		
-		to_display = select_all(conn, "Pressure")
-		Label(window, text = "Pressure: ", bg="none", fg="black", font="none 12 bold") .grid(row=3, column = 0, sticky=W)
-		Label(window, text = to_display, bg="none", fg="black", font="none 12") .grid(row=3, column = 1, sticky=W)
-		
-		to_display = select_all(conn, "Altitude")
-		Label(window, text = "Altitude: ", bg="none", fg="black", font="none 12 bold") .grid(row=4, column = 0, sticky=W)
-		Label(window, text = to_display, bg="none", fg="black", font="none 12") .grid(row=4, column = 1, sticky=W)
-		
+        with conn:
+                to_display = select_all(conn, "Altitude");
+                print "Altitude: " + to_display[2] + " taken at time " + to_display[1]
+
+                to_display = select_all(conn, "Pressure");
+                print "Pressure: " +  to_display[2] + " taken at time " + to_display[1]
+
+                to_display = select_all(conn, "Temperature")
+                print "Temperature: "+ to_display[2] + " taken at time " + to_display[1]
+
+                to_display = select_all(conn, "Light")
+                print "Light: " + to_display[2] + " taken at time " + to_display[1]
+
+                while True:
+                        input = raw_input("Which sensor would you like to see: ")
+                        to_display = select_table(conn, input)
+                        print to_display
 
 if __name__ == '__main__':
     main()
